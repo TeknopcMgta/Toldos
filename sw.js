4f8e5acd22e81c83030bf985c9bfc0235cac2751
TeknopcMgta <123615872+TeknopcMgta@users.noreply.github.com>
2026-09-08 14:56:18 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('Toldos-store').then((cache) => cache.addAll([
+    caches.open('Reservas-store').then((cache) => cache.addAll([
       './',
       './index.html',
       './manifest.json'
